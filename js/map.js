b51aed4c70d27737b97c468551964f6b1c6aa4bd
Daniel Foré <daniel@elementary.io>
2015-09-12 10:26:45 -0700

    populate markers from array

--- js/map.js
+++ js/map.js
@@ -1,30 +1,28 @@
 function initMap() {
 
-    var LatLngList = new Array (new google.maps.LatLng (38.6502574,-121.0627888), new google.maps.LatLng (38.729082,-120.796454));
+    var LatLngList = new Array (new google.maps.LatLng (38.6502574,-121.0627888),
+                                new google.maps.LatLng (38.729082,-120.796454));
+
     var bounds = new google.maps.LatLngBounds ();
 
     var map = new google.maps.Map(document.getElementById('map'), {
-    scrollwheel: false,
-    zoom: 12,
+        zoom: 12,
+        panControl: false,
+        mapTypeControl: false,
+        zoomControl: true
     });
 
-    var marker = new google.maps.Marker({
-    map: map,
-    position: LatLngList[0],
-    title: 'El Dorado Hills Farmers Market'
-    });
+    for (var i = 0; i < LatLngList.length; i++) {
 
-    var marker2 = new google.maps.Marker({
-    map: map,
-    position: LatLngList[1],
-    title: 'Placerville Farmers Market'
-    });
+        var marker = new google.maps.Marker({
+            map: map,
+            position: LatLngList[i],
+            title: ''
+        });
 
-
-    for (var i = 0, LtLgLen = LatLngList.length; i < LtLgLen; i++) {
         bounds.extend (LatLngList[i]);
     }
+
     map.setCenter(bounds.getCenter());
     map.fitBounds(bounds); 
-
 }
